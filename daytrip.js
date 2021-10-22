cbe7f210f49b44c4ab1c1b107137ac37fb9182f0
BrandonCarver93 <brandon.t.carver93@gmail.com>
2021-10-22 16:24:49 -0400

    added all arrays

--- daytrip.js
+++ daytrip.js
@@ -1,60 +1,12 @@
 "use strict"
 
 let destinations = ['Atlanta', 'Savannah', 'Charleston', 'Tybee', 'Ellijay', 'Dahlonega', 'Helen'];
+let restaurants = ['McDonalds', 'BurgerKing', 'Subway', 'Shanes', 'TacoBell', 'Checkers', 'Krystal'];
+let transportation = ['Walk', 'Scooter', 'Uber', 'Bike', 'Drive', 'Boat','Bus'];
+let entertainment = ['Live Music', 'Movies', 'Bars', 'Put-Put', 'Shopping', 'Sight Seeing'];
+
 let random = Math.floor(Math.random() * destinations.length);
 console.log(random, destinations[random]);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function displayGreeting(timeOfDay, aName) {
-    console.log(`Good ${timeOfDay}, ${aName}`);
-}
-
-function determineTimeOfDay(clockTime) {
-    let timeOfDay;
-    if (clockTime < 1200) {
-        timeOfDay = "morning";
-    }   else if (clockTime < 1800) {
-        timeOfDay = "afternoon";
-    }   else {
-        timeOfDay = "evening";
-    }
-    return timeOfDay;
-}
-
-let aName = "Brandon"
-let timeOfDayForToday = determineTimeOfDay(1400);
-displayGreeting(timeOfDayForToday, aName);
 
