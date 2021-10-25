fc171fc48269e10597e044bd4e20974dbf048945
BrandonCarver93 <brandon.t.carver93@gmail.com>
2021-10-25 16:52:05 -0400

    added if and else if for user input

--- daytrip.js
+++ daytrip.js
@@ -1,5 +1,4 @@
 "use strict"
-
 let destinations = ['Atlanta', 'Savannah', 'Charleston', 'Tybee', 'Ellijay', 'Dahlonega', 'Helen'];
 let restaurants = ['McDonalds', 'BurgerKing', 'Subway', 'Shanes', 'TacoBell', 'Checkers', 'Krystal'];
 let transportation = ['Walk', 'Scooter', 'Uber', 'Bike', 'Drive', 'Boat','Bus'];
@@ -35,10 +34,37 @@ function randEntertainment(fun){
         return entertainmentTime
 }
 let letsHaveFun = randEntertainment(entertainment)
-console.log(letsHaveFun);
+console.log(letsHaveFun); 
 
+function myFunction(placeSelected, letsEatHere, letsDrive, letsHaveFun,){
+    alert("Destination:" + ' ' + placeSelected + "\nRestaurant:" + ' ' + letsEatHere + "\nTransportation:" + ' ' + letsDrive + "\nEntertainment:" + ' ' + letsHaveFun)
+}
+ myFunction(placeSelected,letsEatHere,letsDrive,letsHaveFun);
 
+let userInput = prompt("would you like to make any changes YES or NO") 
+    if (userInput === "yes") {
+        let userChange = prompt("What would you like to change? Destination, Restaurant, Transportation, Entertainment")
+        if(userChange === "Destination"){
+            placeSelected = randDestination(destinations)
+            myFunction(placeSelected, letsEatHere, letsDrive, letsHaveFun);
+        } else if (userChange === "Restaurant"){
+            letsEatHere = randRestaurants(restaurants)
+            myFunction(placeSelected, letsEatHere, letsDrive, letsHaveFun);
+        } else if (userChange === "Transportation"){
+            letsDrive = randTransportation(transportation)
+            myFunction(placeSelected, letsEatHere, letsDrive, letsHaveFun);
+        } else if (userChange === "Entertainment"){
+            letsHaveFun = randEntertainment(entertainment)
+            myFunction(placeSelected, letsEatHere, letsDrive, letsHaveFun);
+        } else {
+        myFunction(placeSelected,letsEatHere,letsDrive,letsHaveFun,);
+        }
 
-
-alert("Destination: \nRestaurant: \nTransportation \nEntertainment")
-prompt("would you like to make any changes?")
+     let userConfirm = prompt("Are you Happy with your changes? Yes or No")
+        if(userConfirm === "yes"){
+        myFunction(placeSelected,letsEatHere,letsDrive,letsHaveFun, + 'Enjoy your trip');
+        }
+    } 
+    
+    
+    
